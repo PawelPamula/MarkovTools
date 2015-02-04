805544dec9fcd2dca4ae8fe290efa75b7bf1eab5
Paweł Pamuła <pawel.pamula@gmail.com>
2015-02-04 20:39:51 +0100

    Removed unnecessary println

--- src/permutations.jl
+++ src/permutations.jl
@@ -1,7 +1,6 @@
 function count_fixed_points(array)
     result = 0
     for (index, value) in enumerate(array)
-        println(index, " ", value)
         if index == value
             result += 1
         end
