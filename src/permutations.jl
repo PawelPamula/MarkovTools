32133a7dbc462ca4401a41d88a54413ef62d1c7e
Paweł Pamuła <pawel.pamula@gmail.com>
2015-02-04 20:55:15 +0100

    Added count_cycles function

--- src/permutations.jl
+++ src/permutations.jl
@@ -7,3 +7,23 @@ function count_fixed_points(array)
     end
     return result
 end
+
+
+function count_cycles(array)
+    result = 0
+    visited = trues(size(array, 1))
+    for (index, value) in enumerate(array)
+        if visited[index]
+            j = index
+            while true
+                visited[j] = false
+                j = array[j]
+                if j == index
+                    break
+                end
+            end
+            result += 1
+        end
+    end
+    return result
+end
