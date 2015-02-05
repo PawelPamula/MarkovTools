e436aacf2bc2ea9d860138354c5578a3be66e6c8
Paweł Pamuła <pawel.pamula@gmail.com>
2015-02-05 14:40:26 +0100

    Added tests for count_cycles function

--- test/test_permutations.jl
+++ test/test_permutations.jl
@@ -4,6 +4,7 @@
 using MarkovTools
 using Base.Test
 
+# count_fixed_points
 p1 = []
 @test isequal(count_fixed_points(p1), 0)
 
@@ -19,3 +20,13 @@ p1 = [5, 4, 3, 2, 1]
 N = 1e6
 p1 = [1:N]
 @test isequal(count_fixed_points(p1), N)
+
+# count_cycles
+p1 = []
+@test isequal(count_cycles(p1), 0)
+
+p1 = [1, 2, 3, 4, 5]
+@test isequal(count_cycles(p1), 5)
+
+p1 = [2, 1, 3, 5, 4]
+@test isequal(count_cycles(p1), 3)
