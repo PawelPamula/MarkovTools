7bc8863fb8dc2dfa0f6f266be1655f6546939d1d
Paweł Pamuła <pawel.pamula@gmail.com>
2015-02-06 04:10:34 +0100

    Created pseudo-random number generation submodule.
    - added tests for aformentioned submodule
    - new format of tests using FactCheck.jl
    - added REQUIRE file

--- src/MarkovTools.jl
+++ src/MarkovTools.jl
@@ -3,8 +3,11 @@ module MarkovTools
 export 
     #functions
     count_fixed_points,
-    count_cycles
+    count_cycles,
+
+    readbitstream
 
 include("permutations.jl")
+include("prng/input.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,13 @@ using MarkovTools
 using Base.Test
 
 tests = [
-    "test_permutations"
+    "test_permutations",
+    "prng/test_input"
 ]
+
+if haskey(ENV, "JL_LOAD_PATH")
+    push!(LOAD_PATH, ENV["test/pnrg"])
+end
 
 for t in tests
     test_fn = "$t.jl"
