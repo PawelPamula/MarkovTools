26d5b1c83fd9253ea0924dfbfd78f2d4a91d8892
Pawel Lorek <Pawel.Lorek@math.uni.wroc.pl>
2015-03-24 17:47:16 +0100

    Implementacja algorytmu Spritz cd.

--- src/MarkovTools.jl
+++ src/MarkovTools.jl
@@ -4,10 +4,18 @@ export
     #functions
     count_fixed_points,
     count_cycles,
+    readbitstream,
 
-    readbitstream
+    min,
+    min2,
+    Spritz_Encrypt,
+    Spritz_Decrypt
+    
 
 include("permutations.jl")
 include("prng/input.jl")
+include("rozwiazanie.jl")
+include("rozwiazanie2.jl")
+include("spritz.jl")
 
 end # module
