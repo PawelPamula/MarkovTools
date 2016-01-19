c9bd50e3d99e784f80706833b96a238d92a698da
Marcin Słowik <me@marandil.pl>
2016-01-20 00:36:50 +0100

    Added simple test for biased 2D Gambler

---
+++ src/prng_gambler/Tests.jl
@@ -0,0 +1,23 @@
+#!/usr/bin/env julia
+push!(LOAD_PATH, ".")
+
+using Gambler
+using RandSources
+
+function biasedRandomSource() return rand(0:1) <= 0.9 end
+
+function tests()
+	# Create a new Gambler2D object
+	Ps = [0.25, 0.25]
+	Qs = Ps
+	G2D = Gambler.GamblerND(2, [10,10], [20,20], Ps, Qs)
+	BT = RandSources.BitTrackerND(biasedRandomSource)
+	out = Gambler.runGambler(G2D, Gambler.stepRegular, BT)
+	println(out)
+#			stepWin::AbstractArray{Int64} = [1 for _ in 1:N], 
+#			stepLoss::AbstractArray{Int64} = [-1 for _ in 1:N], 
+#			stepNone::AbstractArray{Int64} = [0 for _ in 1:N])
+end
+
+tests()
+
